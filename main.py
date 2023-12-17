9246cb549d36cfda9220fa5eb15b42e1c7bc8864
SebasGallego <sebasglha@gmail.com>
2023-12-17 18:54:38 -0500

    etructura inicial

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from stored_functions import *
 from stored_procedures import *
 
 
-connection = cx_Oracle.connect(user="BATMAN", password="batroot", dsn="localhost/xe")
+connection = cx_Oracle.connect(user="GASELIN", password="rootG", dsn="localhost/xe")
 cursor = connection.cursor()
 
 while True:    
-
     print("1. Ver el historial de viajes de un camión")
     print("2. Procedimiento 2")
     print("3. Procedimiento 3")
@@ -20,39 +19,41 @@ while True:
     opt = input()
 
     match opt:
-        case str(1): 
+        case '1':
             SP_HISTORIAL_VIAJES_CAMION()
+        case '2':
+            print("Opción 2 seleccionada")
+        case '0':
+            quit()
+        case _:
+            print("Opción no válida")
+
+    try:
+
+        # # Ejecutar una función
+        # function_result = cursor.callfunc('nombre_funcion', cx_Oracle.NUMBER, ['parametro1', 'parametro2'])
+        # print(f'Resultado de la función: {function_result}')
+
+        # # Ejecutar un procedimiento almacenado
+        # procedure_params = {'parametro1': 'valor1', 'parametro2': 'valor2'}
+        # cursor.callproc('nombre_procedimiento', keywordParameters=procedure_params)
+
+        # # Confirmar los cambios en la base de datos
+        # connection.commit()
 
 
 
-
-
-
-
-try:
-
-    # # Ejecutar una función
-    # function_result = cursor.callfunc('nombre_funcion', cx_Oracle.NUMBER, ['parametro1', 'parametro2'])
-    # print(f'Resultado de la función: {function_result}')
-
-    # # Ejecutar un procedimiento almacenado
-    # procedure_params = {'parametro1': 'valor1', 'parametro2': 'valor2'}
-    # cursor.callproc('nombre_procedimiento', keywordParameters=procedure_params)
-
-    # # Confirmar los cambios en la base de datos
-    # connection.commit()
-
-
-
-    # Resto de tu código aquí...
-    
-    sql_query = "SELECT * FROM CAMIONES"
-    ola = cursor.execute(sql_query)
-    for r in ola: 
-        print(r)
-except Exception as e:
-    print(e)
-finally:
-    # Cerrar el cursor y la conexión
-    cursor.close()
-    connection.close()
+        # Resto de tu código aquí...
+        
+        sql_query = "SELECT * FROM CAMIONES"
+        ola = cursor.execute(sql_query)
+        for r in ola: 
+            print(r)
+            
+    except Exception as e:
+        print(e)
+    finally:
+        # Cerrar el cursor y la conexión
+        cursor.close()
+        connection.close()
+        
