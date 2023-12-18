904863c47a146af43a426374b27ce80700b64911
SebasGallego <sebasglha@gmail.com>
2023-12-18 10:11:02 -0500

    stored functions

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+import cx_Oracle
 from triggers import *
 from stored_functions import * 
 from stored_procedures import *
 
 
-
-
-
-
 try:
-    results = ObtenerTiemposUltimoViaje()
+
+    results = FnCargaMasTransportada()
     print(results)
 except Exception as e:
     print(e)
@@ -16,68 +14,58 @@ finally:
     # Cerrar el cursor y la conexión
     cursor.close()
     connection.close()
+#
+
+##
+#while True:    
+#    print("1. Ver el historial de viajes de un camión")
+#    print("2. Procedimiento 2")
+#    print("3. Procedimiento 3")
+#    print("4. Función 1")
+#    print("5. Función 2")
+#    print("0. Salir")
+#
+#
+#    opt = input()
+#
+#    match opt:
+#        case '1':
+#            SP_HISTORIAL_VIAJES_CAMION()
+#        case '2':
+#            print("Opción 2 seleccionada")
+#        case '0':
+#            quit()
+#        case _:
+#            print("Opción no válida")
+#
+#    try:
+#
+#        # # Ejecutar una función
+#        # function_result = cursor.callfunc('nombre_funcion', cx_Oracle.NUMBER, ['parametro1', 'parametro2'])
+#        # print(f'Resultado de la función: {function_result}')
+#
+#        # # Ejecutar un procedimiento almacenado
+#        # procedure_params = {'parametro1': 'valor1', 'parametro2': 'valor2'}
+#        # cursor.callproc('nombre_procedimiento', keywordParameters=procedure_params)
+#
+#        # # Confirmar los cambios en la base de datos
+#        # connection.commit()
+#
+#
+#
+#        # Resto de tu código aquí...
+#        
+#        sql_query = "SELECT * FROM CAMIONES"
+#        ola = cursor.execute(sql_query)
+#        for r in ola: 
+#            print(r)
+#            
+#    except Exception as e:
+#        print(e)
+#    finally:
+#        # Cerrar el cursor y la conexión
+#        cursor.close()
+#        connection.close()
+#
 
 
-
-
-
-
-# while True:    
-
-#     print("1. Ver el historial de viajes de un camión")
-#     print("2. Procedimiento 2")
-#     print("3. Procedimiento 3")
-#     print("4. Función 1")
-#     print("5. Función 2")
-#     print("0. Salir")
-connection = cx_Oracle.connect(user="GASELIN", password="rootG", dsn="localhost/xe")
-cursor = connection.cursor()
-
-while True:    
-    print("1. Ver el historial de viajes de un camión")
-    print("2. Procedimiento 2")
-    print("3. Procedimiento 3")
-    print("4. Función 1")
-    print("5. Función 2")
-    print("0. Salir")
-
-
-#     opt = input()
-
-    match opt:
-        case '1':
-            SP_HISTORIAL_VIAJES_CAMION()
-        case '2':
-            print("Opción 2 seleccionada")
-        case '0':
-            quit()
-        case _:
-            print("Opción no válida")
-
-    try:
-
-        # # Ejecutar una función
-        # function_result = cursor.callfunc('nombre_funcion', cx_Oracle.NUMBER, ['parametro1', 'parametro2'])
-        # print(f'Resultado de la función: {function_result}')
-
-        # # Ejecutar un procedimiento almacenado
-        # procedure_params = {'parametro1': 'valor1', 'parametro2': 'valor2'}
-        # cursor.callproc('nombre_procedimiento', keywordParameters=procedure_params)
-
-        # # Confirmar los cambios en la base de datos
-        # connection.commit()
-
-
-
-    # Resto de tu código aquí...
-    
-    sql_query = "SELECT * FROM CAMIONES"
-    ola = cursor.execute(sql_query)
-    for r in ola: 
-        print(r)
-except Exception as e:
-    print(e)
-finally:
-    # Cerrar el cursor y la conexión
-    cursor.close()
-    connection.close()
--- stored_functions.py
+++ stored_functions.py
@@ -8,3 +8,23 @@ def ObtenerTiemposUltimoViaje():
     print("Ingrese la placa del camión el cual quiere consultar su tiempo del último viaje realizado")
     placa = input()
     return cursor.callfunc("ObtenerTiemposUltimoViaje", cx_Oracle.STRING, [placa])
+
+
+def origenViaje():
+    print("Ingrese la placa del camión el cual quiere consultar su origen del último viaje realizado")
+    placa = input()
+    return cursor.callfunc("FnOrigenViaje", cx_Oracle.STRING, [placa])
+
+def FnDetalleCarga():
+    print("Ingrese la placa del camión el cual quiere consultar el detalle de la carga")
+    placa = input()
+    return cursor.callfunc("FnDetalleCarga", cx_Oracle.STRING, [placa])
+
+def FnCiudadMasViajes():
+    print("se está consultando la ciudad con más viajes...")
+    return cursor.callfunc("FnCiudadMasViajes", cx_Oracle.STRING, [])
+
+
+def FnCargaMasTransportada():
+    print("se está consultando la carga más transportada...")
+    return cursor.callfunc("FnCargaMasTransportada", cx_Oracle.STRING, [])
