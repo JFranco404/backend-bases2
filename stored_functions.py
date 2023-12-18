58dbacb4805e3496ca21d664e349bd4f2c94ec6f
JFranco404 <jhon.1702011356@ucaldas.edu.co>
2023-12-18 10:48:44 -0500

    Inicio de CLI

--- stored_functions.py
+++ stored_functions.py
@@ -8,3 +8,17 @@ def ObtenerTiemposUltimoViaje():
     print("Ingrese la placa del camión el cual quiere consultar su tiempo del último viaje realizado")
     placa = input()
     return cursor.callfunc("ObtenerTiemposUltimoViaje", cx_Oracle.STRING, [placa])
+
+
+def DetallesConductor():
+    print("Ingrese la cedula del conductar el cual quiere consultar su información")
+    cedula = input()
+    return cursor.callfunc("DetallesConductor", cx_Oracle.STRING, [cedula])
+
+
+def SF_CONTAR_TURNOS_JORNADA():
+    return cursor.callfunc("SF_CONTAR_TURNOS_JORNADA", cx_Oracle.NUMBER)
+
+
+def SF_CAMION_CON_MAS_VIAJES():
+    return cursor.callfunc("SF_CAMION_CON_MAS_VIAJES", cx_Oracle.STRING)
